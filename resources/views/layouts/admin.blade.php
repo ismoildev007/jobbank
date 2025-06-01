f1f7ebc218d7dfc4a790b950f953916400b01093
mehriddin <xalilov.mehri@gmail.com>
2025-06-01 13:36:34 +0500

    fix admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,120 +1,265 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+
+
+<html lang="en" class=" layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-skin="default"
+      data-bs-theme="light" data-assets-path="/admin/assets/" data-template="vertical-menu-template">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MAD Parfüm </title>
-    <meta property="og:site_name" content="MAD Parfüm – Let&#39;s Get Mad! Kadın Parfüm, Erkek Parfüm" /><meta property="og:description" content="" /><meta property="og:image" content="" /><meta property="og:title" content="Niche Parfüm Alana Hediye!" /><meta property="og:type" content="product.group" /><meta property="og:url" content="https://www.madparfum.com/niche-parfum-alana-hediye" />    <link rel='preload' href='https://static.ticimax.cloud/51735/CustomCss/ticimax/style.css?v=8.13.252.0.125237' as='style' />
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <title>Demo: Dashboard - Analytics | Materialize - Bootstrap Dashboard PRO</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="../../css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="/admin/plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="../../ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="/admin/plugins/jqvmap/jqvmap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="/admin/dist/css/adminlte.min.css?v=3.2.0">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="/admin/plugins/daterangepicker/daterangepicker.css">
-    <!-- summernote -->
-    <link rel="stylesheet" href="/admin/plugins/summernote/summernote-bs4.min.css">
 
-    <link rel="stylesheet" href="/admin/plugins/fontawesome-free/css/all.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="/admin/dist/css/adminlte.min.css?v=3.2.0">
-    <!-- summernote -->
-    <link rel="stylesheet" href="./admin/plugins/summernote/summernote-bs4.min.css">
-    <!-- CodeMirror -->
-    <link rel="stylesheet" href="/admin/plugins/codemirror/codemirror.css">
-    <link rel="stylesheet" href="/admin/plugins/codemirror/theme/monokai.css">
-    <!-- SimpleMDE -->
-    <link rel="stylesheet" href="/admin/plugins/simplemde/simplemde.min.css">
+    <meta name="description"
+          content="Materialize is the best bootstrap 5 dashboard for responsive web apps. Streamline your app development process with ease.">
+    <!-- Canonical SEO -->
+    <meta name="keywords"
+          content="Materialize bootstrap dashboard, Materialize bootstrap 5 dashboard, themeselection, html dashboard, web dashboard, frontend dashboard, responsive bootstrap theme">
+    <meta property="og:title" content="Materialize bootstrap Dashboard by Pixinvent">
+    <meta property="og:type" content="product">
+    <meta property="og:url" content="https://themeforest.net/item/materialize-material-design-admin-template/11446068">
+    <meta property="og:image"
+          content="https://ts-assets.b-cdn.net/pi-assets/materialize/landing-page/materialize-hero-image.png">
+    <meta property="og:description"
+          content="Materialize is the best bootstrap 5 dashboard for responsive web apps. Streamline your app development process with ease.">
+    <meta property="og:site_name" content="Pixinvent">
+    <link rel="canonical" href="https://themeforest.net/item/materialize-material-design-admin-template/11446068">
+
+
+    <!-- ? PROD Only: Google Tag Manager (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
+    <script>
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5J3LMKC');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="/admin/assets/img/favicon/favicon.ico">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="../../../css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/admin/assets/vendor/fonts/iconify-icons.css">
+
+    <!-- Core CSS -->
+    <!-- build:css assets/vendor/css/theme.css -->
+
+    <link rel="stylesheet" href="/admin/assets/vendor/libs/node-waves/node-waves.css">
+
+
+    <link rel="stylesheet" href="/admin/assets/vendor/libs/pickr/pickr-themes.css">
+
+    <link rel="stylesheet" href="/admin/assets/vendor/css/core.css">
+    <link rel="stylesheet" href="/admin/assets/css/demo.css">
+
+
+    <!-- Vendors CSS -->
+
+    <link rel="stylesheet" href="/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+
+    <!-- endbuild -->
+
+    <link rel="stylesheet" href="/admin/assets/vendor/libs/apex-charts/apex-charts.css">
+    <link rel="stylesheet" href="/admin/assets/vendor/libs/swiper/swiper.css">
+
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="/admin/assets/vendor/css/pages/cards-statistics.css">
+
+    <!-- Helpers -->
+    <script src="/admin/assets/vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js. -->
+    <script src="/admin/assets/vendor/js/template-customizer.js"></script>
+
+    <!--? Config: Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file. -->
+
+    <script src="/admin/assets/js/config.js"></script>
+
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
 
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+<body>
+
+<!-- ?PROD Only: Google Tag Manager (noscript) (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5J3LMKC" height="0" width="0"
+            style="display: none; visibility: hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<!-- Layout wrapper -->
+<div class="layout-wrapper layout-content-navbar  ">
+    <div class="layout-container">
+
+
+        <!-- Menu -->
+
+        <aside id="layout-menu" class="layout-menu menu-vertical menu">
+
+            <div class="app-brand demo ">
+                <a href="index.html" class="app-brand-link">
+      <span class="app-brand-logo demo">
+  <span class="text-primary">
+    <svg width="32" height="18" viewbox="0 0 38 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+          d="M30.0944 2.22569C29.0511 0.444187 26.7508 -0.172113 24.9566 0.849138C23.1623 1.87039 22.5536 4.14247 23.5969 5.92397L30.5368 17.7743C31.5801 19.5558 33.8804 20.1721 35.6746 19.1509C37.4689 18.1296 38.0776 15.8575 37.0343 14.076L30.0944 2.22569Z"
+          fill="currentColor"></path>
+      <path
+          d="M30.171 2.22569C29.1277 0.444187 26.8274 -0.172113 25.0332 0.849138C23.2389 1.87039 22.6302 4.14247 23.6735 5.92397L30.6134 17.7743C31.6567 19.5558 33.957 20.1721 35.7512 19.1509C37.5455 18.1296 38.1542 15.8575 37.1109 14.076L30.171 2.22569Z"
+          fill="url(#paint0_linear_2989_100980)" fill-opacity="0.4"></path>
+      <path
+          d="M22.9676 2.22569C24.0109 0.444187 26.3112 -0.172113 28.1054 0.849138C29.8996 1.87039 30.5084 4.14247 29.4651 5.92397L22.5251 17.7743C21.4818 19.5558 19.1816 20.1721 17.3873 19.1509C15.5931 18.1296 14.9843 15.8575 16.0276 14.076L22.9676 2.22569Z"
+          fill="currentColor"></path>
+      <path
+          d="M14.9558 2.22569C13.9125 0.444187 11.6122 -0.172113 9.818 0.849138C8.02377 1.87039 7.41502 4.14247 8.45833 5.92397L15.3983 17.7743C16.4416 19.5558 18.7418 20.1721 20.5361 19.1509C22.3303 18.1296 22.9391 15.8575 21.8958 14.076L14.9558 2.22569Z"
+          fill="currentColor"></path>
+      <path
+          d="M14.9558 2.22569C13.9125 0.444187 11.6122 -0.172113 9.818 0.849138C8.02377 1.87039 7.41502 4.14247 8.45833 5.92397L15.3983 17.7743C16.4416 19.5558 18.7418 20.1721 20.5361 19.1509C22.3303 18.1296 22.9391 15.8575 21.8958 14.076L14.9558 2.22569Z"
+          fill="url(#paint1_linear_2989_100980)" fill-opacity="0.4"></path>
+      <path
+          d="M7.82901 2.22569C8.87231 0.444187 11.1726 -0.172113 12.9668 0.849138C14.7611 1.87039 15.3698 4.14247 14.3265 5.92397L7.38656 17.7743C6.34325 19.5558 4.04298 20.1721 2.24875 19.1509C0.454514 18.1296 -0.154233 15.8575 0.88907 14.076L7.82901 2.22569Z"
+          fill="currentColor"></path>
+      <defs>
+        <lineargradient id="paint0_linear_2989_100980" x1="5.36642" y1="0.849138" x2="10.532" y2="24.104"
+                        gradientunits="userSpaceOnUse">
+          <stop offset="0" stop-opacity="1"></stop>
+          <stop offset="1" stop-opacity="0"></stop>
+        </lineargradient>
+        <lineargradient id="paint1_linear_2989_100980" x1="5.19475" y1="0.849139" x2="10.3357" y2="24.1155"
+                        gradientunits="userSpaceOnUse">
+          <stop offset="0" stop-opacity="1"></stop>
+          <stop offset="1" stop-opacity="0"></stop>
+        </lineargradient>
+      </defs>
+    </svg>
+  </span>
+</span>
+                    <span class="app-brand-text demo menu-text fw-semibold ms-2">Materialize</span>
+                </a>
+
+                <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+                    <svg width="24" height="24" viewbox="0 0 24 24" fill="currentColor"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M8.47365 11.7183C8.11707 12.0749 8.11707 12.6531 8.47365 13.0097L12.071 16.607C12.4615 16.9975 12.4615 17.6305 12.071 18.021C11.6805 18.4115 11.0475 18.4115 10.657 18.021L5.83009 13.1941C5.37164 12.7356 5.37164 11.9924 5.83009 11.5339L10.657 6.707C11.0475 6.31653 11.6805 6.31653 12.071 6.707C12.4615 7.09747 12.4615 7.73053 12.071 8.121L8.47365 11.7183Z"
+                            fill-opacity="0.9"></path>
+                        <path
+                            d="M14.3584 11.8336C14.0654 12.1266 14.0654 12.6014 14.3584 12.8944L18.071 16.607C18.4615 16.9975 18.4615 17.6305 18.071 18.021C17.6805 18.4115 17.0475 18.4115 16.657 18.021L11.6819 13.0459C11.3053 12.6693 11.3053 12.0587 11.6819 11.6821L16.657 6.707C17.0475 6.31653 17.6805 6.31653 18.071 6.707C18.4615 7.09747 18.4615 7.73053 18.071 8.121L14.3584 11.8336Z"
+                            fill-opacity="0.4"></path>
+                    </svg>
+                </a>
+            </div>
+
+            <div class="menu-inner-shadow"></div>
+
+
+            <x-admin.sidebar></x-admin.sidebar>
+
+        </aside>
+
+        <div class="menu-mobile-toggler d-xl-none rounded-1">
+            <a href="javascript:void(0);"
+               class="layout-menu-toggle menu-link text-large text-bg-secondary p-2 rounded-1">
+                <i class="ri ri-menu-line icon-base"></i>
+                <i class="ri ri-arrow-right-s-line icon-base"></i>
+            </a>
+        </div>
+        <!-- / Menu -->
+
+
+        <!-- Layout container -->
+        <div class="layout-page">
+
+
+            <x-admin.header></x-admin.header>
+
+
+            <!-- Content wrapper -->
+            <div class="content-wrapper">
+                @yield('content')
+
+                <x-admin.footer></x-admin.footer>
+
+
+                <div class="content-backdrop fade"></div>
+            </div>
+            <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
     </div>
 
-    <!-- Navbar -->
-    <x-admin.header></x-admin.header>
 
-    <!-- /.navbar -->
-
-    <x-admin.sidebar></x-admin.sidebar>
-
-    <!-- Content Wrapper. Contains page content -->
-  @yield('content')
-    <!-- /.content-wrapper -->
-    <x-admin.footer></x-admin.footer>
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
 
 
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+
 </div>
-<!-- ./wrapper -->
+<!-- / Layout wrapper -->
 
-<!-- jQuery -->
-<script src="/admin/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="/admin/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="/admin/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="/admin/plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="/admin/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="/admin/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="/admin/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="/admin/plugins/moment/moment.min.js"></script>
-<script src="/admin/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="/admin/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="/admin/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="/admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="/admin/dist/js/adminlte.js?v=3.2.0"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="/admin/dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/admin/dist/js/pages/dashboard.js"></script>
-<script src="/admin/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="/admin/dist/js/adminlte.min.js?v=3.2.0"></script>
-<!-- Summernote -->
-<script src="/admin/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- CodeMirror -->
-<script src="/admin/plugins/codemirror/codemirror.js"></script>
-<script src="/admin/plugins/codemirror/mode/css/css.js"></script>
-<script src="/admin/plugins/codemirror/mode/xml/xml.js"></script>
-<script src="/admin/plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="/admin/dist/js/demo.js"></script>
-@yield('scripts')
+
+<div class="buy-now">
+    <a href="https://themeforest.net/item/materialize-material-design-admin-template/11446068" target="_blank"
+       class="btn btn-danger btn-buy-now">Buy Now</a>
+</div>
+
+
+<!-- Core JS -->
+
+<!-- build:js assets/vendor/js/theme.js  -->
+
+
+<script src="/admin/assets/vendor/libs/jquery/jquery.js"></script>
+
+<script src="/admin/assets/vendor/libs/popper/popper.js"></script>
+<script src="/admin/assets/vendor/js/bootstrap.js"></script>
+<script src="/admin/assets/vendor/libs/node-waves/node-waves.js"></script>
+
+
+<script src="/admin/assets/vendor/libs/%40algolia/autocomplete-js.js"></script>
+
+
+<script src="/admin/assets/vendor/libs/pickr/pickr.js"></script>
+
+
+<script src="/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+
+<script src="/admin/assets/vendor/libs/hammer/hammer.js"></script>
+
+<script src="/admin/assets/vendor/libs/i18n/i18n.js"></script>
+
+
+<script src="/admin/assets/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="/admin/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+<script src="/admin/assets/vendor/libs/swiper/swiper.js"></script>
+
+<!-- Main JS -->
+
+<script src="/admin/assets/js/main.js"></script>
+
+
+<!-- Page JS -->
+<script src="/admin/assets/js/dashboards-analytics.js"></script>
+
 </body>
 </html>
+
+<!-- beautify ignore:end -->
